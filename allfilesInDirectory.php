27f11ab8be1cc148eaae8750fb084a06f0416c46
LeNGuyenWi <lenguyenwi@gmail.com>
2017-03-15 16:50:20 +0100

    first commit

---
+++ allfilesInDirectory.php
@@ -0,0 +1,19 @@
+<?php
+
+if ($handle = opendir('./DownloadFiles/')) { //  opendir('/path/to/files')) {
+    echo "Directory handle: $handle<br>";
+    echo "Entries:<br>";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        echo "<a href=\"./downloadLitmitRate.php\">".$entry."</a><br>";
+    }
+
+    /* This is the WRONG way to loop over the directory. */
+    // while ($entry = readdir($handle)) {
+    //     echo "$entry\n";
+    // }
+
+    closedir($handle);
+}
+?>
---
+++ downloadLitmitRate.php
@@ -0,0 +1,31 @@
+<?php
+// $local_file = 'file.zip';
+// $download_file = 'name.zip';
+$local_file = './DownloadFiles/logo_ldg.jpg';
+$download_file = 'file.jpg';
+// set the download rate limit (=> 20,5 kb/s)
+$download_rate = 20.5;
+if(file_exists($local_file) && is_file($local_file))
+{
+    header('Cache-control: private');
+    header('Content-Type: application/octet-stream');
+    header('Content-Length: '.filesize($local_file));
+    header('Content-Disposition: filename='.$download_file);
+
+    flush();
+    $file = fopen($local_file, "r");
+    while(!feof($file))
+    {
+        // send the current file part to the browser
+        print fread($file, round($download_rate * 1024));
+        // flush the content to the browser
+        flush();
+        // sleep one second
+        sleep(1);
+    }
+    fclose($file);}
+else {
+    die('Error: The file '.$local_file.' does not exist!');
+}
+
+?>
