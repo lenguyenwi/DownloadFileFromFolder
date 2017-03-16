f39502c8b726cb6ba79b743b85c607409d3e8843
LeNGuyenWi <lenguyenwi@gmail.com>
2017-03-16 13:45:49 +0100

    time2

--- allfilesInDirectory.php
+++ allfilesInDirectory.php
@@ -1,16 +1,21 @@
+<!DOCTYPE html>
 <?php
 
 if ($handle = opendir('./DownloadFiles/')) { //  opendir('/path/to/files')) {
-    echo "Directory handle: $handle<br>";
-    echo "Entries:<br>";
+    // echo "Directory handle: $handle<br>";
+    echo "Please click on the file to download<br>";
+    // echo "Entries:<br>";
 
     /* This is the correct way to loop over the directory. */
     while (false !== ($entry = readdir($handle))) {
 
         // echo "<a href=\"./downloadLitmitRate.php\">".$entry."</a><br>";
+        if(file_exists($entry)!=true)
+        {
           echo "<a href=\"./downloadLitmitRate.php?local_file=$entry\">".$entry."</a><br>";
+        }
     }
-
+    // echo "<button>".'Download all'."</button>";
     /* This is the WRONG way to loop over the directory. */
     // while ($entry = readdir($handle)) {
     //     echo "$entry\n";
